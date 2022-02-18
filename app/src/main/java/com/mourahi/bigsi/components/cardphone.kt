01b3f81ef24939075c0884a465dbea130e12ba43
mourahi <adil.mourahi@gmail.com>
2022-02-18 14:35:26 +0100

    PhONE CARD

--- app/src/main/java/com/mourahi/bigsi/components/cardphone.kt
+++ app/src/main/java/com/mourahi/bigsi/components/cardphone.kt
@@ -1,21 +1,25 @@
 package com.mourahi.bigsi.components
 
 import android.util.Log
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.CheckBox
-import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.*
 import androidx.compose.material.icons.outlined.CheckBoxOutlineBlank
 import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.mourahi.bigsi.phones.Phone
 import com.mourahi.bigsi.ui.theme.myPadding
 
@@ -27,9 +31,10 @@ fun PhonePageContent(
     onEdit: (ph:Phone) -> Unit,
     onUpdate: (ph: Phone) -> Unit
     ) {
+    val colla = remember{ mutableStateOf(-1) }
     LazyColumn{
-            items(phones){
-                CardPhone(it,onCardOperations, onEdit, onUpdate)
+            itemsIndexed(phones){index,item ->
+                CardPhone(item,onCardOperations, onEdit, onUpdate,index,colla)
             }
 
 
@@ -37,7 +42,8 @@ fun PhonePageContent(
 }
 
 @Composable
- fun CardPhone(ph: Phone, oncardOperation: MutableState<Boolean>, onEdit:(ph:Phone)->Unit, onUpdate: (ph: Phone) -> Unit)
+ fun CardPhone(ph: Phone, oncardOperation: MutableState<Boolean>,
+               onEdit:(ph:Phone)->Unit, onUpdate: (ph: Phone) -> Unit,index:Int,colla:MutableState<Int>)
  {
     Log.d("adil","CardPHone: phone=$ph")
     Card(
@@ -46,49 +52,84 @@ fun PhonePageContent(
             .padding(top = myPadding, start = myPadding, end = myPadding),
         elevation = myPadding
     ) {
-        Row(
+        Column(
             Modifier
-                .fillMaxSize()
-                .padding(myPadding),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ){
-            Column {
-                Text(text = ph.ecole )
-                Text(text = ph.nom)
-            }
+                .fillMaxWidth()
+                .padding(myPadding)){
+            Row(
+                Modifier
+                    .fillMaxSize()
+                    .clickable {
+                        colla.value = if(colla.value == index) -1 else index
+                    },
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(Icons.Filled.Phone, contentDescription = "PHone", tint = Color.Red)
+                }
 
-            Row(verticalAlignment = Alignment.CenterVertically) {
+                Column {
+                    Text(text = ph.ecole + "spand =${ colla.value == index}")
+                    Text(text = ph.nom)
+                }
+
+                Row(verticalAlignment = Alignment.CenterVertically) {
 
 
                     MyToggleIcon(
                         selectFirst = false, // todo:a completer
-                        icons = listOf(Icons.Filled.Edit) ) {
+                        icons = listOf(Icons.Filled.Edit)
+                    ) {
                         //todo: edit personnel phoneViewModel.phones.value (open page Phone for edit)
-                       onEdit(ph)
+                        onEdit(ph)
                         return@MyToggleIcon ""
                     }
 
 
 
-              MyToggleIcon(
-                  selectFirst = ph.fav,
-                    icons = listOf(Icons.Filled.Favorite,Icons.Outlined.FavoriteBorder) ) {
-                 ph.fav = !ph.fav
-                 onUpdate(ph)
-                  return@MyToggleIcon ""
-                }
+                    MyToggleIcon(
+                        selectFirst = ph.fav,
+                        icons = listOf(Icons.Filled.Favorite, Icons.Outlined.FavoriteBorder)
+                    ) {
+                        ph.fav = !ph.fav
+                        onUpdate(ph)
+                        return@MyToggleIcon ""
+                    }
 
-             if(oncardOperation.value)  MyToggleIcon(
-                  selectFirst =  !ph.isChecked , // todo:a completer
-                    icons = listOf(Icons.Outlined.CheckBoxOutlineBlank,Icons.Filled.CheckBox) )
-               {
-                   ph.isChecked = !ph.isChecked
-                   onUpdate(ph)
-                   return@MyToggleIcon ""
+                    if (oncardOperation.value) MyToggleIcon(
+                        selectFirst = !ph.isChecked, // todo:a completer
+                        icons = listOf(Icons.Outlined.CheckBoxOutlineBlank, Icons.Filled.CheckBox)
+                    )
+                    {
+                        ph.isChecked = !ph.isChecked
+                        onUpdate(ph)
+                        return@MyToggleIcon ""
+                    }
                 }
             }
-
+            if( colla.value == index) Column(Modifier.fillMaxWidth()){
+                Text(text = ph.tel)
+                Row(
+                    Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                )
+                {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(Icons.Filled.Send, contentDescription = "send", tint = Color.Black)
+                    }
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(Icons.Filled.Mail, contentDescription = "mail", tint = Color.Black)
+                    }
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(Icons.Filled.MapsHomeWork, contentDescription = "Map", tint = Color.Black
+                        )
+                    }
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(Icons.Filled.PlusOne, contentDescription = "Plus", tint = Color.Black)
+                    }
+                }
+            }
         }
     }
 }
